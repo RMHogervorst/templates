2d1a4e699d16582431f7b787940dd0b1a70ab3ee
Roel Hogervorst <RMHogervorst@users.noreply.github.com>
2016-07-04 10:16:02 +0200

    update test_dummy_proofing with philosophy

--- test_dummy_proofing.R
+++ test_dummy_proofing.R
@@ -8,6 +8,14 @@
 # That your outputs are what you expect them to be
 # That you get reasonable answers for reasonable inputs
 # When you know what the answer should be - you get it
+# self contained (don't rely on code outside, except the function you are testing)
+# isolated: don't rely on other testcases  (although I do declare at top of script and test it further on)
+# unique: test what has not been tested
+# useful: focus on edge cases and inputs where the function may behave erratically or wildly
+# Mock out all external services and state 
+# Don’t unit-test configuration settings
+# Name your unit tests clearly and consistently
+# When you find a bug, first write a test for it and then fix the bug.
 
 # examples #####
 
@@ -23,12 +31,17 @@ test_that("wrong imput values throw informative error" ,{
 })
 
 ## output proofing
-
+test_that("output is of the type expected" , {
+  expect_type(object, type)
+})
 # results  what should be
+### add standard cases, most expected input types 
 
-# edge cases
+### edge cases
+# infinity / zero / 1 / 1000 times more then usual
 
-# random inputs
+### random inputs
+#use random generator
 
 
 ###  Tip:  try to break your functions: enormous files, inf and -inf as input, Null or NA as input. 
